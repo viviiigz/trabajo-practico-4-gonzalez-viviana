8e3062878ff031fa00141d5942548751f1e4c9a8
Viviana Gonzalez <vivigz06@gmail.com>
2025-06-26 15:23:28 -0300

    se realizaron todas las validaciones

--- src/controllers/character.controllers.js
+++ src/controllers/character.controllers.js
@@ -3,13 +3,13 @@ import Character from "../models/character.model";
 
 //funcion para enviar errores de validoacion
 const validacionError= async(req,res) =>{
-    return res.status(400).json({error:error.message})
+    return res.status(400).json({error:message})
 };
 
 //get perosonajes
 export const getCharacter = async(req,res) => {
     try{
-        const character = Character.findAll();//para todos los personajes
+        const character = await Character.findAll();//para todos los personajes
         res.json(character);
     } catch (error) {
         res.status(500).json({error: "Error al encontrar personajes"})
@@ -20,9 +20,9 @@ export const getCharacter = async(req,res) => {
 //get personajes id
 export const getCharacterId = async(req,res)=> {
     try{
-        const character = Character.findByPk(req.params.id);
+        const character = await Character.findByPk(req.params.id);
        if (character) res.json(character);
-       else res.status(400).json({error:"No se encontraron personajes que coincidan"
+       else res.status(404).json({error:"No se encontraron personajes que coincidan"
         })
     } catch (error){
         res.status(500).json({error:error.message})
@@ -41,20 +41,61 @@ export const getCharacterId = async(req,res)=> {
         }
 //Validar que ki sea un número entero válido, es decir, que el valor no sea un string ni un
 //número con decimales (float). Solo se deben aceptar valores que sean de tipo entero.
-        if (!Number.isInterger(ki) || ki < 0){
+        if (!Number.isInteger(ki) || ki < 0){
             return validacionError(res, 'Ki debe ser un número entero válido')
         }
 //Validar que el campo gender solo acepte los valores "Male" o "Female". Cualquier otro
 //valor debe considerarse inválido y debe generar un mensaje de error.
-       if (!['Male', 'Female'].includes(gender)){
-        return validacionError(res, 'Género debe ser "Male" o "Female"');
-       }
-
+         if (!['Male', 'Female'].includes(gender)){
+            return validacionError(res, 'Género debe ser "Male" o "Female"');
+        }
  //asegurarase que description sea una cadena si se incluye
- if(description !== undefined && typeof description !== 'string'){
-    return validacionError(res, 'La descripción debe ser una cadena si se proporciona.')
+         if(description !== undefined && typeof description !== 'string'){
+            return validacionError(res, 'La descripción debe ser una cadena si se proporciona.')
  }   
+  const existingCharacter = await Character.findOne({ where: { name } });
+     if (existingCharacter && existingCharacter.id !== parseInt(id)) {
+      return validacionError(res, 'Ya existe un personaje con este nombre.');
+      }
+
+  const character = await Character.create({name,ki,race,gender,description})
+  res.status(201).json(character);
+} catch(error){
+    res.status(500).json({error:'Error interno del servidor'});
+};
+}
+
+//Verificar que el campo name sea único 
+export const updateCharacter = async (req,res)=> {
+    const {id} = req.params; //extraemos
+    const {name, ki, race, gender, description} = req.body; //actuaalizamos
+
+    try { //buscamos el persoanje por id
+        const character = await Character.findByPk(id);
+
+        if (!character){
+            return res.status(404).json({error:"Personaje no encontrado"})
+        }
+
+    //el nombre no debe estar vacio y si es distinto del actual verificar
+        if (!name && name !== character.name){
+            const existeCharacter = await Character.findOne({where:{name}});
+            if (existeCharacter && existeCharacter.id !== character.id) {
+                return res.status(400).json({ error: 'Ya existe otro personaje con este nombre.' });
+            }
+        }
+     //actualizar el personaje con los datos recibidos
+    await character.update({ name, ki, race, gender, description });
+    res.status(200).json(character);
+
+  } catch (error) {
+    // Manejo de error por violación de unicidad en Sequelize
+    if (error.name === 'SequelizeUniqueConstraintError') {
+      return res.status(400).json({ error: 'Ya existe un personaje con este nombre.' });
     }
- };
+    res.status(500).json({ error: 'Error interno del servidor al actualizar el personaje.' });
+    };
+};
+
 
 
